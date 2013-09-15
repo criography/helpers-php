19a3bb27b4a5b8359f9ab3dd86852be35b828c78
criography <reg@criography.com>
2013-09-15 19:06:38 +0100

    Added prefix support, renamed multiply function

--- colour/blending.php
+++ colour/blending.php
@@ -2,11 +2,12 @@
 
 
 	/**
-	 * getOrangeImg
+	 * getColourMultipliedImg
 	 * creates an image with colour multiply blending mode. Can be cropped.
 	 *
 	 * @uses Imagick
 	 * @param string $srcURL Full PATH from the docroot (no domain)
+	 * @param string $suffix string attached to the filename. Defaults to '-blended'.
 	 * @param string $colour hex value of a colour to be used for blending
 	 * @param int $width destination width
 	 * @param int $height destination height
@@ -16,11 +17,11 @@
 	 * @return string path to the blended image
 	 */
 
-	function getOrangeImg($srcURL, $colour='#ff0000', $width, $height, $valign, $jpg_quality=75, $forceOverwrite=false){
+	function getColourMultipliedImg($srcURL, $suffix='-blended', $colour='#ff0000', $width, $height, $valign, $jpg_quality=75, $forceOverwrite=false){
 
-		$blendedURL  = preg_replace('/(\.jpg|\.jpeg)/i', '-orange$1', $srcURL);    /* absolute to the docroot, without the domain name */
-		$srcPath    = $_SERVER['DOCUMENT_ROOT'].$srcURL;                          /* absolute to the  server root */  
-		$blendedPath = $_SERVER['DOCUMENT_ROOT'].$blendedURL;                       /* absolute to the  server root */
+		$blendedURL   = preg_replace('/(\.jpg|\.jpeg)/i', $suffix.'$1', $srcURL);   /* absolute to the docroot, without the domain name */
+		$srcPath      = $_SERVER['DOCUMENT_ROOT'].$srcURL;                          /* absolute to the server root */
+		$blendedPath  = $_SERVER['DOCUMENT_ROOT'].$blendedURL;                      /* absolute to the server root */
 
 
 		if(!file_exists($blendedPath) || (file_exists($blendedPath) && $forceOverwrite===true) ){
@@ -39,9 +40,12 @@
 				$y = 0;
 				if($valign==='middle'){
 					$y = round(($img->getimageheight() - $height) * .5);
+
 				}else if($valign==='bottom'){
 					$y = $img->getImageHeight() - $height;
+
 				}
+
 
 				/* crop image*/
 				$img->cropImage($width, $height, $x, $y);
@@ -59,7 +63,7 @@
 				/* compress image */
 				$img->setImageCompression(Imagick::COMPRESSION_JPEG);
 				$img->setImageCompressionQuality($jpg_quality);
-				$img->setSamplingFactors(array(2,1,1));
+				$img->setSamplingFactors(array(2, 1, 1));
 				$img->stripImage();
 				$img->normalizeImage();
 				$img->unsharpMaskImage(0 , 0.75 , 1 , 0.05);
